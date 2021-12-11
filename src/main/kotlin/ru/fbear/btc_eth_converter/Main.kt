2766ebe5c1cd21840731b806d004df8c18138ac7
teacons <teacons@fbear.ru>
2021-12-11 04:19:45 +0300

    Implemented a web server for converting cryptocurrencies.

--- src/main/kotlin/ru/fbear/btc_eth_converter/Main.kt
+++ src/main/kotlin/ru/fbear/btc_eth_converter/Main.kt
@@ -2,9 +2,11 @@ package ru.fbear.btc_eth_converter
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.sun.net.httpserver.HttpServer
 import java.io.File
 import java.io.FileNotFoundException
 import java.net.HttpURLConnection
+import java.net.InetSocketAddress
 import java.net.URL
 
 
@@ -14,21 +16,23 @@ const val coin_from = "BTC"
 
 const val coin_to = "ETH"
 
+var apiKey = ""
+
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
-        println("Usage: java -jar btc-eth-converter.jar number_of_coins")
+        println("Usage: java -jar btc-eth-converter.jar port")
         return
     }
 
-    val numOfCoin = args[0].toIntOrNull()
+    val port = args[0].toIntOrNull()
 
-    if (numOfCoin == null) {
-        println("Usage: java -jar btc-eth-converter.jar number_of_coins")
+    if (port == null) {
+        println("Usage: java -jar btc-eth-converter.jar port")
         return
     }
 
-    val apiKey =
+    apiKey =
         try {
             File("api_key.txt").bufferedReader().readText()
         } catch (e: FileNotFoundException) {
@@ -36,7 +40,53 @@ fun main(args: Array<String>) {
             return
         }
 
-    val url = URL("$api_url?amount=$numOfCoin&symbol=$coin_from&convert=$coin_to")
+    val server = HttpServer.create(InetSocketAddress(port), 0)
+
+    server.createContext("/api/convert") {
+        val params =
+            if (it.requestURI.query != null)
+                it.requestURI.query.split('&')
+            else emptyList()
+
+        if (params.size != 1) {
+            it.sendResponseHeaders(400, -1)
+            it.close()
+            return@createContext
+        }
+
+        val param = params.first()
+
+        val paramName = param.split('=').first()
+
+        if (paramName != "amount") {
+            it.sendResponseHeaders(400, -1)
+            it.close()
+            return@createContext
+        }
+
+        val paramValue = param.split('=').last()
+
+        val amount = convertByApi(paramValue)
+
+        val respText = amount.toString()
+
+        it.sendResponseHeaders(200, respText.length.toLong())
+
+        it.responseBody.apply {
+            write(respText.toByteArray(Charsets.UTF_8))
+            flush()
+        }
+    }
+
+    server.executor = null
+
+    server.start()
+
+
+}
+
+fun convertByApi(amount: String): Double {
+    val url = URL("$api_url?amount=$amount&symbol=$coin_from&convert=$coin_to")
 
     val con = url.openConnection() as HttpURLConnection
 
@@ -48,5 +98,5 @@ fun main(args: Array<String>) {
 
     val response: Response = jacksonObjectMapper().readValue(con.inputStream)
 
-    println("Price: ${response.data.quote.eth.price}")
+    return response.data.quote.eth.price
 }
